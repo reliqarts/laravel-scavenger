2f891fbede622d2aa12f13b45c7f4ebeafca8765
Reliq <reliq@reliqarts.com>
2022-03-01 23:23:22 +0100

    support Laravel 9 (#4)

--- ecs.php
+++ ecs.php
@@ -3,13 +3,14 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(SetList::CLEAN_CODE);
+    $containerConfigurator->import(SetList::PSR_12);
+
     $parameters = $containerConfigurator->parameters();
-
-    $parameters->set('line_ending', "\n");
-
-    $parameters->set('paths', [__DIR__ . '/src', __DIR__ . '/tests']);
-
-    $parameters->set('sets', ['clean-code', 'psr12']);
+    $parameters->set(Option::LINE_ENDING, "\n");
+    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
 };
